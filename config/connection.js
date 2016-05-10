31d80d78d091f32933829e85c4f7516c1174a3b7
SWERVE <swerve@CracBook-Pro.local>
2016-05-10 18:35:49 -0400

    modified connection.js withn jawsDB code

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1111',
     database: 'burgers_db'
-});
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
